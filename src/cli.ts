59b21a0f7ece752016de379648e13246d8c0acad
cevin <qixingyue@gmail.com>
2025-11-13 10:16:19 +0800

    test common job

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
+
+import dotenv from "dotenv";
+dotenv.config();
+
 const cliMain = async () => {
-  console.log("Hello World from CLI");
+  const value = process.env.MY_SECRET_KEY;
+  console.log("Hello World from CLI", value);
 };
 cliMain();
